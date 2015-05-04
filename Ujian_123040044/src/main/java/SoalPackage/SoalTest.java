304f2a93a749c5f6af7e887042084dce9234cc3d
nur.sofia@mail.unpas.ac.id <>
2015-05-04 11:53:37 +0700

    coba comit

---
+++ Ujian_123040044/src/main/java/SoalPackage/SoalTest.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SoalPackage;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author SB 604-17
+ */
+public class SoalTest {
+    private Soal S;
+   
+    
+   
+    
+    
+}
